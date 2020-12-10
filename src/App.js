4adb60a60bf7761d1f62d029c7fd3a8dbf3676d5
peterjjonas <peterjjonas@tutanota.com>
2020-12-10 18:02:56 +0000

    First version of the app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
-import React, { Component} from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import Scroll from './Scroll.js';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchfield: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users}));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value})
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    const filteredMonsters = this.state.monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
+    if (this.state.monsters.length === 0) {
+      return <h1 className='tc f1'>Loading</h1>
+    } else {
+        return (
+          <div className='tc'>
+            <h1 className='f1'>Monsterfriends</h1>
+            <SearchBox searchChange={this.onSearchChange}/>
+            <Scroll>
+              <CardList monsters={filteredMonsters}/>
+            </Scroll>
+          </div>
+      );
+    }
   }
 }
 
