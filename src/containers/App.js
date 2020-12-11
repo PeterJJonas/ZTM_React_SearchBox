dd700035e8ed3abfa92245d592c756b45b9e32cb
peterjjonas <peterjjonas@tutanota.com>
2020-12-11 14:12:37 +0000

    Clean code

---
+++ src/containers/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+// import Scroll from '../components/Scroll.js';
+import Stayput from '../components/Stayput';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchfield: ''
+    }
+  }
+
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => this.setState({ monsters: users}));
+}
+
+onSearchChange = (event) => {
+  this.setState({ searchfield: event.target.value})
+}
+
+render() {
+  const { monsters, searchfield } = this.state;
+  const filteredMonsters = monsters.filter(monster => {
+    const searchIn = (monster.name.toLowerCase() + monster.username.toLowerCase());
+    return searchIn.toLowerCase().includes(searchfield.toLowerCase());
+  })
+  return !monsters.length ?
+    <h1 className='tc f1'>Loading</h1> :
+    (
+      <div className='tc'>
+        <Stayput>
+          <h1 className='f1'>Monsterfriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+        </Stayput>
+        <CardList monsters={filteredMonsters}/>
+      </div>
+    );
+  }
+}
+
+export default App;
